563e514158f969cc694ce0d513b199dcfca3963a
juanglc <juanguiloco3@gmail.com>
2025-11-10 20:57:09 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
