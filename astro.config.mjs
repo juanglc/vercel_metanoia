4859695087ee3d85328c2a38284a276de1c98527
juanglc <juanguiloco3@gmail.com>
2025-11-10 20:09:27 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://cuartetometanoia.com',
+  site: 'https://vercel-metanoia.vercel.app',
   output: 'server',
   adapter: vercel(),
     integrations: [
