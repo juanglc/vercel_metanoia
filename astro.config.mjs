10371b65df34728cbf2a0a63bca3c4534cc09f4f
Juan Guillermo López <juanguiloco3@gmail.com>
2025-11-11 13:49:59 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel'; // <-- ESTA ES LA CORRECTA
+import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
